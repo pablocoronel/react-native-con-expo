2f1332fcb669c82f306e66628db8c2d20e741e72
Pablo Coronel <pablomanuelcoronel@gmail.com>
2021-06-06 22:26:52 -0300

    feat: add styles

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 const App = () => {
 	return (
-		<View
-			style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
-		>
-			<Text style={{ fontSize: 30 }}>Hola</Text>
+		<View style={styles.container}>
+			<Text style={styles.title}>Hola</Text>
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#292929',
+	},
+	title: { fontSize: 30, color: '#fff' },
+});
 
 export default App;
