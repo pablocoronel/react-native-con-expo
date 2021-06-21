9fbf03d8152642426b8b4979c667ef5ae836ed99
Pablo Coronel <pablomanuelcoronel@gmail.com>
2021-06-21 17:23:22 -0300

    feat: use buttons

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
+import {
+	Text,
+	View,
+	StyleSheet,
+	Image,
+	Button,
+	Alert,
+	TouchableOpacity,
+} from 'react-native';
 // import huella from './assets/huella.png';
 
 const App = () => {
@@ -7,11 +15,26 @@ const App = () => {
 		<View style={styles.container}>
 			<Text style={styles.title}>Hola</Text>
 
+			{/* Source soporta url web y local (importandolo) */}
 			<Image
 				source={{ uri: 'https://picsum.photos/200/200' }}
 				// source={huella}
 				style={styles.image}
 			/>
+
+			{/* Button para botón básico, y TouchableOpacity para uno personalizable */}
+			<Button
+				color="000"
+				title="Press me"
+				onPress={() => Alert.alert('Hola')}
+			/>
+
+			<TouchableOpacity
+				onPress={() => Alert.alert('Hola')}
+				style={styles.button}
+			>
+				<Text style={styles.buttonText}>Press me custom</Text>
+			</TouchableOpacity>
 		</View>
 	);
 };
@@ -25,6 +48,8 @@ const styles = StyleSheet.create({
 	},
 	title: { fontSize: 30, color: '#fff' },
 	image: { width: 200, height: 200, borderRadius: 100 },
+	button: { backgroundColor: 'blue', padding: 7, marginTop: 10 },
+	buttonText: { color: '#FFF', fontSize: 20 },
 });
 
 export default App;
