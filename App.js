2f83231bac7c3d41fa72d6cba6573576595b4b2e
Pablo Coronel <pablomanuelcoronel@gmail.com>
2021-06-21 17:01:52 -0300

    feat: use images

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image } from 'react-native';
+// import huella from './assets/huella.png';
 
 const App = () => {
 	return (
 		<View style={styles.container}>
 			<Text style={styles.title}>Hola</Text>
+
+			<Image
+				source={{ uri: 'https://picsum.photos/200/200' }}
+				// source={huella}
+				style={styles.image}
+			/>
 		</View>
 	);
 };
@@ -17,6 +24,7 @@ const styles = StyleSheet.create({
 		backgroundColor: '#292929',
 	},
 	title: { fontSize: 30, color: '#fff' },
+	image: { width: 200, height: 200, borderRadius: 100 },
 });
 
 export default App;
